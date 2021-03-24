e0de89589b2821377a43bc8681117c3b9c11b436
janskoruba <jan@skoruba.com>
2021-03-24 20:52:51 +0100

    Fix UTF encoding

--- src/Skoruba.Duende.IdentityServer.STS.Identity/ViewModels/Consent/ProcessConsentResult.cs
+++ src/Skoruba.Duende.IdentityServer.STS.Identity/ViewModels/Consent/ProcessConsentResult.cs
@@ -1,8 +1,8 @@
-// Copyright (c) Duende Software. All rights reserved.
+﻿// Copyright (c) Duende Software. All rights reserved.
 // See LICENSE in the project root for license information.
 
 // Original file: https://github.com/DuendeSoftware/IdentityServer.Quickstart.UI
-// Modified by Jan �koruba
+// Modified by Jan Škoruba
 
 using Duende.IdentityServer.Models;
 
