889b2f65f104d222c685a2801982a9e43934e03c
janskoruba <jan@skoruba.com>
2022-01-03 17:16:12 +0100

    Fix install.ps1

--- templates/install.ps1
+++ templates/install.ps1
@@ -1,7 +1,7 @@
 param([string] $packagesVersions, [string]$gitBranchName = 'dev')
 
-$PSScriptRoot\0-build-template.ps1 -packagesVersions $packagesVersions -gitBranchName $gitBranchName
+.\0-build-template.ps1 -packagesVersions $packagesVersions -gitBranchName $gitBranchName
 
-$PSScriptRoot\1-add-docker-support.ps1
+.\1-add-docker-support.ps1
 
-$PSScriptRoot\2-publish-template.ps1 -packagesVersions $packagesVersions
+.\2-publish-template.ps1 -packagesVersions $packagesVersions
