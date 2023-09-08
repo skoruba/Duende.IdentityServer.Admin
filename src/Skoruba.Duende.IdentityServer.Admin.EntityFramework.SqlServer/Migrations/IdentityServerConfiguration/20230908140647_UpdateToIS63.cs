f1539c89e30aa389861de6af097676c3d2dee0bf
colin younge <colin.younge@wearefreemarket.com>
2023-09-08 15:33:24 +0100

    Ensure compatiblity with 6.3

---
+++ src/Skoruba.Duende.IdentityServer.Admin.EntityFramework.SqlServer/Migrations/IdentityServerConfiguration/20230908140647_UpdateToIS63.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Skoruba.Duende.IdentityServer.Admin.EntityFramework.SqlServer.Migrations.IdentityServerConfiguration
+{
+    public partial class UpdateToIS63 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "DPoPClockSkew",
+                table: "Clients",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+
+            migrationBuilder.AddColumn<int>(
+                name: "DPoPValidationMode",
+                table: "Clients",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InitiateLoginUri",
+                table: "Clients",
+                type: "nvarchar(2000)",
+                maxLength: 2000,
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "RequireDPoP",
+                table: "Clients",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DPoPClockSkew",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "DPoPValidationMode",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "InitiateLoginUri",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "RequireDPoP",
+                table: "Clients");
+        }
+    }
+}
