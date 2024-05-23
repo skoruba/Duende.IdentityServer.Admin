786933d0140b06d9aa719466948ffc296cf33c91
Jan Škoruba <jan@skoruba.com>
2024-05-23 12:13:32 +0200

    Change Swashbuckle to NSwag and add support for generation of TS client for SPA applications.

--- src/Skoruba.Duende.IdentityServer.Admin.Api/Configuration/Authorization/AuthorizeCheckOperationFilter.cs
+++ src/Skoruba.Duende.IdentityServer.Admin.Api/Configuration/Authorization/AuthorizeCheckOperationFilter.cs
@@ -4,44 +4,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.OpenApi.Models;
-using Swashbuckle.AspNetCore.SwaggerGen;
-
+using NSwag;
+using NSwag.Generation.Processors;
+using NSwag.Generation.Processors.Contexts;
 namespace Skoruba.Duende.IdentityServer.Admin.Api.Configuration.Authorization
 {
-    public class AuthorizeCheckOperationFilter : IOperationFilter
+    public class AuthorizeCheckOperationProcessor : IOperationProcessor
     {
         private readonly AdminApiConfiguration _adminApiConfiguration;
 
-        public AuthorizeCheckOperationFilter(AdminApiConfiguration adminApiConfiguration)
+        public AuthorizeCheckOperationProcessor(AdminApiConfiguration adminApiConfiguration)
         {
             _adminApiConfiguration = adminApiConfiguration;
         }
-        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+
+        public bool Process(OperationProcessorContext context)
         {
-            var hasAuthorize = context.MethodInfo.DeclaringType != null && (context.MethodInfo.DeclaringType.GetCustomAttributes(true).OfType<AuthorizeAttribute>().Any()
-                                                                            || context.MethodInfo.GetCustomAttributes(true).OfType<AuthorizeAttribute>().Any());
+            var hasAuthorize = context.ControllerType.GetCustomAttributes(true).OfType<AuthorizeAttribute>().Any() ||
+                               context.MethodInfo.GetCustomAttributes(true).OfType<AuthorizeAttribute>().Any();
 
             if (hasAuthorize)
             {
-                operation.Responses.Add("401", new OpenApiResponse { Description = "Unauthorized" });
-                operation.Responses.Add("403", new OpenApiResponse { Description = "Forbidden" });
+                context.OperationDescription.Operation.Responses.Add("401", new OpenApiResponse { Description = "Unauthorized" });
+                context.OperationDescription.Operation.Responses.Add("403", new OpenApiResponse { Description = "Forbidden" });
 
-                operation.Security = new List<OpenApiSecurityRequirement>
+                var oauth2Scheme = new OpenApiSecurityRequirement
                 {
-                    new OpenApiSecurityRequirement
-                    {
-                        [
-                            new OpenApiSecurityScheme {Reference = new OpenApiReference
-                                {
-                                    Type = ReferenceType.SecurityScheme,
-                                    Id = "oauth2"}
-                            }
-                        ] = new[] { _adminApiConfiguration.OidcApiName }
-                    }
+                    ["OAuth2"] = new[] { _adminApiConfiguration.OidcApiName }
                 };
 
+                context.OperationDescription.Operation.Security.Add(oauth2Scheme);
             }
+
+            return true;
         }
     }
 }
