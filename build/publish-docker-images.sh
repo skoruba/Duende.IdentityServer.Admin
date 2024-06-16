667c1c4fd5873a31345db71dd9bc9255fb52be20
Jan Škoruba <jan@skoruba.com>
2024-06-16 19:56:22 +0200

    Polish README and fix publish docker script

--- build/publish-docker-images.sh
+++ build/publish-docker-images.sh
@@ -2,12 +2,12 @@
 
 # Ensure a version is provided
 if [ -z "$1" ]; then
-  echo "Usage: $0 <version> <platform>"
+  echo "Usage: $0 <version> <platforms>"
   exit 1
 fi
 
 version=$1
-platform=${2:-linux/amd64}  # Default to linux/amd64 if not provided
+platforms=${2:-linux/amd64,linux/arm64}  # Default to linux/amd64 and linux/arm64 if not provided
 
 # Ensure buildx is set up
 docker buildx create --use
@@ -15,22 +15,7 @@ docker buildx create --use
 # Change directory to the project root
 cd ..
 
-# Build and load docker images with platform specified and verify architecture
-docker buildx build --platform $platform -t skoruba-duende-identityserver-admin:latest --load --no-cache -f src/Skoruba.Duende.IdentityServer.Admin/Dockerfile .
-docker inspect --format '{{.Os}}/{{.Architecture}}' skoruba-duende-identityserver-admin:latest
-
-docker buildx build --platform $platform -t skoruba-duende-identityserver-admin-api:latest --load --no-cache -f src/Skoruba.Duende.IdentityServer.Admin.Api/Dockerfile .
-docker inspect --format '{{.Os}}/{{.Architecture}}' skoruba-duende-identityserver-admin-api:latest
-
-docker buildx build --platform $platform -t skoruba-duende-identityserver-sts-identity:latest --load --no-cache -f src/Skoruba.Duende.IdentityServer.STS.Identity/Dockerfile .
-docker inspect --format '{{.Os}}/{{.Architecture}}' skoruba-duende-identityserver-sts-identity:latest
-
-# Tag images with the specified version
-docker tag skoruba-duende-identityserver-admin:latest skoruba/duende-identityserver-admin:$version
-docker tag skoruba-duende-identityserver-admin-api:latest skoruba/duende-identityserver-admin-api:$version
-docker tag skoruba-duende-identityserver-sts-identity:latest skoruba/duende-identityserver-sts-identity:$version
-
-# Push images to Docker Hub
-docker push skoruba/duende-identityserver-admin:$version
-docker push skoruba/duende-identityserver-admin-api:$version
-docker push skoruba/duende-identityserver-sts-identity:$version
+# Build and push docker images with platforms specified
+docker buildx build --platform $platforms -t skoruba/duende-identityserver-admin:latest -t skoruba/duende-identityserver-admin:$version --push --no-cache -f src/Skoruba.Duende.IdentityServer.Admin/Dockerfile .
+docker buildx build --platform $platforms -t skoruba/duende-identityserver-admin-api:latest -t skoruba/duende-identityserver-admin-api:$version --push --no-cache -f src/Skoruba.Duende.IdentityServer.Admin.Api/Dockerfile .
+docker buildx build --platform $platforms -t skoruba/duende-identityserver-sts-identity:latest -t skoruba/duende-identityserver-sts-identity:$version --push --no-cache -f src/Skoruba.Duende.IdentityServer.STS.Identity/Dockerfile .
