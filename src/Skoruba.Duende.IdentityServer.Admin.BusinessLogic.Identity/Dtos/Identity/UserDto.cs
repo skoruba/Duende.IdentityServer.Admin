91b4b66099117ed3cbd8fe23b4a2f121087ce38f
rosenbach <30808341+rosenbach@users.noreply.github.com>
2024-10-24 15:24:58 +0200

    chore(username-validation): undo reordering of usings

--- src/Skoruba.Duende.IdentityServer.Admin.BusinessLogic.Identity/Dtos/Identity/UserDto.cs
+++ src/Skoruba.Duende.IdentityServer.Admin.BusinessLogic.Identity/Dtos/Identity/UserDto.cs
@@ -1,10 +1,10 @@
 ﻿// Copyright (c) Jan Škoruba. All Rights Reserved.
 // Licensed under the Apache License, Version 2.0.
 
-using Skoruba.Duende.IdentityServer.Admin.BusinessLogic.Identity.Dtos.Identity.Base;
-using Skoruba.Duende.IdentityServer.Admin.BusinessLogic.Identity.Dtos.Identity.Interfaces;
 using System;
 using System.ComponentModel.DataAnnotations;
+using Skoruba.Duende.IdentityServer.Admin.BusinessLogic.Identity.Dtos.Identity.Base;
+using Skoruba.Duende.IdentityServer.Admin.BusinessLogic.Identity.Dtos.Identity.Interfaces;
 
 namespace Skoruba.Duende.IdentityServer.Admin.BusinessLogic.Identity.Dtos.Identity
 {
