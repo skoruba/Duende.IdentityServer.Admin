86ff96a6036cfbc7997b2b2bace1dfda2d2bb459
albert0ly <albert.l@infobaysec.com>
2025-06-25 12:33:40 +0300

    SMS Interface

---
+++ src/SMSSender/LogSMSSender.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace SMSSender
+{
+    public class LogSMSSender: ISMSSender
+    {
+        private readonly ILogger<LogSMSSender> _logger;
+
+        public LogSMSSender(ILogger<LogSMSSender> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task<string> SendSMSAsync(string number, string message)
+        {
+            _logger.LogInformation($"nubmer: {number} message: {message}");
+
+            return Task.FromResult("");
+        }
+    }
+}
